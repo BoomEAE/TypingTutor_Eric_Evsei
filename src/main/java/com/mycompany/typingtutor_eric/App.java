24665e38144720e2c29c7e389dcfc135a178e3c9
Eric <86203664+BoomEAE@users.noreply.github.com>
2026-09-12 13:26:25 -0700

    Added the next and reset button to the program Updated the visual look of the keyboard Added events that happen when a key is pressed and released Updated the TextField to a label that will/or shows the key pressed

--- src/main/java/com/mycompany/typingtutor_eric/App.java
+++ src/main/java/com/mycompany/typingtutor_eric/App.java
@@ -20,23 +20,68 @@ public class App extends Application {
 
     @Override
     public void start(Stage mainStage) {
-        //
-        char letters[] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
         
         //Creation of root and keys instances
         BorderPane root = new BorderPane();
         GridPane gridPane = new GridPane();
         
         //TextField and Label instances
-        Label keyPressed = new Label("TEST TEST TEST");
-        TextField inputField = new TextField();
+        Label textPhrase = new Label("E");
+        Label keyPressed = new Label();
+        Button nextButton = new Button("next");
+        Button resetButton = new Button("reset");
         
-        //A list that will contain every button needed
-        Button buttons[] = new Button[28];
+        
+        //Create and add buttons
+        Button buttons[] = Create_Add_Buttons(gridPane);
         
         //Add the label and textfield
-        gridPane.add(keyPressed, 5, 0);
-        gridPane.add(inputField, 5, 1);
+        gridPane.add(textPhrase, 5, 0);
+        gridPane.add(keyPressed, 5, 1);
+        gridPane.add(nextButton, 9, 0);
+        gridPane.add(resetButton, 0, 0);
+        
+        //Add the gridPane with all the buttons, textfield and label to the root
+        root.getChildren().add(gridPane);
+        
+        
+        Scene scene = new Scene(root, 200, 200);
+        
+        scene.setOnKeyPressed(event -> {
+            String keyText = event.getCode().getName().toUpperCase();
+            
+            for(Button button : buttons){
+                if(keyText.equals(button.getText().toUpperCase())){
+                    changeButtonStyle(button, 1);
+                }else if(keyText.equals(" ")){
+                    if(button.getText().equals("Space")){
+                        changeButtonStyle(button, 1);
+                    }
+                }
+            }
+        });
+        
+        scene.setOnKeyReleased(event -> {
+            for(Button button : buttons){
+                if(event.getCode().getName().toLowerCase().equals(button.getText().toLowerCase())){
+                    changeButtonStyle(button, 0);
+                }
+            }
+        });
+        
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    
+    //Creates and adds necessary buttons for the typing tutor program
+    public static Button[] Create_Add_Buttons(GridPane gridPane){
+        //
+        char letters[] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+        Button buttons[] = new Button[28];
         
         //For loop that creates button instances corresponding to different keys on a keyboard
         //and then stores them into a list
@@ -44,14 +89,18 @@ public class App extends Application {
             switch(i){
                 case 26:
                     buttons[i] = new Button("Shift");
+                    buttons[i].setMinWidth(50);
                     break;
                 case 27:
                     buttons[i] = new Button("Space");
+                    buttons[i].setMinWidth(50);
                     break;
                 default:
                     buttons[i] = new Button("" + letters[i]);
+                    buttons[i].setMinWidth(30);
                     break;
             }
+            changeButtonStyle(buttons[i], 0);
         }
         
         //For loop to iterate through the buttons list and adds them to the gridPane following the Label and TextField
@@ -63,21 +112,37 @@ public class App extends Application {
             }else if(i >= 19 && i < 26){
                 gridPane.add(buttons[i], i-17, 4);
             }else{
-                gridPane.add(buttons[i], i-22, 5);
+                switch(i){
+                    case 27:
+                       gridPane.add(buttons[i], i-21, 5);
+                       break;
+                    default:
+                        gridPane.add(buttons[i], i-22, 5);
+                }
             }
         }
         
-        //Add the gridPane with all the buttons, textfield and label to the root
-        root.getChildren().add(gridPane);
+        GridPane.setColumnSpan(buttons[26], 2);
+        GridPane.setColumnSpan(buttons[27], 2);
         
-        
-        Scene scene = new Scene(root, 200, 200);
-        mainStage.setScene(scene);
-        mainStage.show();
+        return buttons;
     }
-
-    public static void main(String[] args) {
-        launch();
+    
+    //Sets a style sheet to a button
+    public static void changeButtonStyle(Button button, int sheetNum /*0 being the default stylesheet and 1 being the alternative*/){
+        String defaultCssSheet = "-fx-background-radius: 15; -fx-background-color: #FFD700";
+        String alternativeCssSheet = "-fx-background-radius: 15; -fx-background-color: #CC5500";
+        
+        //We check which sheet as been selected by the program
+        //and set the appropriate style sheet to the button according to the sheet number selected
+        switch(sheetNum){
+            case 0:
+                button.setStyle(defaultCssSheet);
+                break;
+            case 1:
+                button.setStyle(alternativeCssSheet);
+                break;
+        }
     }
 
 }
