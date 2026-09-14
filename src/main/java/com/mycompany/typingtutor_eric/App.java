650badc2f3eacfe43f7209782f81083ce69c5989
Eric <86203664+BoomEAE@users.noreply.github.com>
2026-09-13 17:28:15 -0700

    Fixed the layout of the program so that it's centered at all times
    Label used to display what's being typed by the user now updates whenever they press keys on a physical keyboard. NOTE: There still are issues regarding this which will be fixed such as when the user presses the comma key it adds to the label: "COMMA"
    
    Added key events for the reset and next button

--- src/main/java/com/mycompany/typingtutor_eric/App.java
+++ src/main/java/com/mycompany/typingtutor_eric/App.java
@@ -6,9 +6,10 @@ import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.input.KeyCode;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
 
 
 
@@ -17,50 +18,116 @@ import javafx.scene.input.KeyCode;
  * JavaFX App
  */
 public class App extends Application {
+    
+    private int counter = 0;
 
     @Override
     public void start(Stage mainStage) {
+        //Phrases
+        String phrases[] = {"Try typing this text. Do it as quickly and accurately as you can.",
+                            "Next type another line of input data.",
+                            "The quick brown fox jumps over the lazy `dog.",
+                            "Five big quacking zephyrs jolt my wax bed.",
+                            "Sympathizing would fix Quaker objectives.",
+                            "A large fawn jumped quickly over white zinc boxes"};
         
         //Creation of root and keys instances
         BorderPane root = new BorderPane();
-        GridPane gridPane = new GridPane();
+        GridPane mainGridPane = new GridPane();
+        HBox top = new HBox(95);
+        GridPane center = new GridPane();
+        GridPane bottom = new GridPane();
         
         //TextField and Label instances
-        Label textPhrase = new Label("E");
-        Label keyPressed = new Label();
+        Label textPhrase = new Label(phrases[0]);
+        Label textCounter = new Label("1 of 6");
+        Label keyPressed = new Label("[Text typed will show here]");
         Button nextButton = new Button("next");
         Button resetButton = new Button("reset");
         
-        
         //Create and add buttons
-        Button buttons[] = Create_Add_Buttons(gridPane);
+        Button buttons[] = Create_Add_Buttons(bottom);
         
         //Add the label and textfield
-        gridPane.add(textPhrase, 5, 0);
-        gridPane.add(keyPressed, 5, 1);
-        gridPane.add(nextButton, 9, 0);
-        gridPane.add(resetButton, 0, 0);
+        top.getChildren().addAll(resetButton, textCounter, nextButton);
+        center.add(textPhrase, 5, 1);
+        center.add(keyPressed, 5, 2);
+        mainGridPane.add(top, 0, 0);
+        mainGridPane.add(center, 0, 1);
+        mainGridPane.add(bottom, 0, 2);
+        
+        textPhrase.setMinWidth(200);
+        
+        //Modifie a few columns
+        GridPane.setColumnSpan(buttons[26], 2);
+        GridPane.setColumnSpan(buttons[27], 2);
+        GridPane.setColumnSpan(nextButton, 2);
+        GridPane.setColumnSpan(textCounter, 3);
+        GridPane.setColumnSpan(resetButton, 2);
+        
+        
+        //Add margins so that each sections have space in between
+        GridPane.setMargin(top, new Insets(0, 0, 20, 0));
+        GridPane.setMargin(center, new Insets(0, 0, 20, 0));
         
         //Add the gridPane with all the buttons, textfield and label to the root
-        root.getChildren().add(gridPane);
+        mainGridPane.setAlignment(Pos.CENTER);
+        root.setCenter(mainGridPane);
         
+        //
+        nextButton.setOnAction(event -> {
+            counter++;
+            textCounter.setText((counter+1) + " of 6");
+            textPhrase.setText(phrases[counter]);
+        });
         
+        //
+        resetButton.setOnAction(event -> {
+            counter = 0;
+            textCounter.setText("0 of 6");
+            textPhrase.setText(phrases[counter]);
+        });
+        
+        //
         Scene scene = new Scene(root, 200, 200);
         
+        //
         scene.setOnKeyPressed(event -> {
             String keyText = event.getCode().getName().toUpperCase();
+            System.out.println(keyText);
+            if(keyPressed.getText().equals("[Text typed will show here]")){
+                keyPressed.setText("");
+            }
+            
+            //
+            if(keyText.equals("BACKSPACE")){
+                int textLength = keyPressed.getText().length();
+                if(textLength > 0){
+                    String subText = keyPressed.getText().substring(0, textLength-1);
+                    
+                    if(subText.length() == 0){
+                        keyPressed.setText("[Text typed will show here]");
+                    }else{
+                        keyPressed.setText(subText);
+                    }
+                }
+            }else{
+                keyPressed.setText(keyPressed.getText() + keyText);
+            }
+            
             
             for(Button button : buttons){
                 if(keyText.equals(button.getText().toUpperCase())){
                     changeButtonStyle(button, 1);
-                }else if(keyText.equals(" ")){
-                    if(button.getText().equals("Space")){
+                }else if(button.getText().equals("Space")){
+                    if(keyText.equals(" ")){
                         changeButtonStyle(button, 1);
                     }
                 }
             }
         });
         
+        //
         scene.setOnKeyReleased(event -> {
             for(Button button : buttons){
                 if(event.getCode().getName().toLowerCase().equals(button.getText().toLowerCase())){
@@ -106,24 +173,21 @@ public class App extends Application {
         //For loop to iterate through the buttons list and adds them to the gridPane following the Label and TextField
         for(int i = 0; i < buttons.length; i++){
             if(i >= 0 && i < 10){
-                gridPane.add(buttons[i], i, 2);
+                gridPane.add(buttons[i], i, 0);
             }else if(i >= 10 && i < 19){
-                gridPane.add(buttons[i], i-9, 3);
+                gridPane.add(buttons[i], i-9, 1);
             }else if(i >= 19 && i < 26){
-                gridPane.add(buttons[i], i-17, 4);
+                gridPane.add(buttons[i], i-17, 2);
             }else{
                 switch(i){
                     case 27:
-                       gridPane.add(buttons[i], i-21, 5);
+                       gridPane.add(buttons[i], i-21, 3);
                        break;
                     default:
-                        gridPane.add(buttons[i], i-22, 5);
+                        gridPane.add(buttons[i], i-22, 3);
                 }
             }
         }
-        
-        GridPane.setColumnSpan(buttons[26], 2);
-        GridPane.setColumnSpan(buttons[27], 2);
         
         return buttons;
     }
